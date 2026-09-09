17ba9765a0fadf0a43d2aee0202b0f6b5b541654
eugenioenko <eugenioenko@gmail.com>
2026-09-08 20:36:31 -0700

    chore: remove omitempty from trasnaprentBackground

--- internal/config/settings.go
+++ internal/config/settings.go
@@ -110,7 +110,7 @@ type EditorSettings struct {
 	ShowTrailingNewline     *bool  `json:"showTrailingNewline,omitempty"`
 	MenuBar                 *bool  `json:"menuBar,omitempty"`
 	UndoDeleteCursorStart   bool   `json:"undoDeleteCursorStart,omitempty"`
-	TransparentBackground   bool   `json:"transparentBackground,omitempty"`
+	TransparentBackground   bool   `json:"transparentBackground"`
 }
 
 func (e EditorSettings) IsShowTrailingNewlineEnabled() bool {
