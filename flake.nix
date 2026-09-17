bf629f107c033d693fafecc18acfb88202e7c0de
eugenioenko <17834212+eugenioenko@users.noreply.github.com>
2026-09-17 16:51:05 +0000

    chore: auto-update vendorHash in flake.nix

--- flake.nix
+++ flake.nix
@@ -18,7 +18,7 @@
             pname = "ttt";
             inherit version;
             src = self;
-            vendorHash = "sha256-QKunMv+rv+a4ps+yXZVxr/MTOkp6c8mslH2BsHozIsY=";
+            vendorHash = "sha256-1h4qVll2nxmNBe7aqEJ2I0u5a1jsSJV3WXel1jan4yw=";
                           
             ldflags = [
               "-s"
